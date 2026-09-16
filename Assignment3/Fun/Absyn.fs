e112c45d3a3c23512be480c77e8d926e8bed0a22
Kakbob <jakobcjuhl@gmail.com>
2026-09-17 01:22:35 +0200

    4.3 done

--- Assignment3/Fun/Absyn.fs
+++ Assignment3/Fun/Absyn.fs
@@ -9,5 +9,6 @@ type expr =
   | Let of string * expr * expr
   | Prim of string * expr * expr
   | If of expr * expr * expr
-  | Letfun of string * string * expr * expr    (* (f, x, fBody, letBody) *)
-  | Call of expr * expr
+  //Exercise 4.2 extended to take in multiple variables in letfun
+  | Letfun of string * string list * expr * expr    (* (f, xs, fBody, letBody) *)
+  | Call of expr * expr list (* (f, xs) *)
--- Assignment3/Fun/Fun.fs
+++ Assignment3/Fun/Fun.fs
@@ -24,7 +24,9 @@ let rec lookup env x =
 
 type value = 
   | Int of int
-  | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  //Exercise 4.3
+  //we modified closure to take a list of xs instead of x
+  | Closure of string * string list * expr * value env       (* (f, xs, fBody, fDeclEnv) *)
 
 let rec eval (e : expr) (env : value env) : int =
     match e with 
@@ -52,17 +54,30 @@ let rec eval (e : expr) (env : value env) : int =
       let b = eval e1 env
       if b<>0 then eval e2 env
       else eval e3 env
-    | Letfun(f, x, fBody, letBody) -> 
-      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env 
+
+      //Exercise 4.3
+      //We change the x to xs to reflect the change we made in Absyn.fs and Closure
+    | Letfun(f, xs, fBody, letBody) -> 
+      let bodyEnv = (f, Closure(f, xs, fBody, env)) :: env 
       eval letBody bodyEnv
-    | Call(Var f, eArg) -> 
+
+      //We change the call function to reflect the change we made in Absyn.fs and CLosure
+    // | Call(Var f, eArg) -> 
+    //   let fClosure = lookup env f
+    //   match fClosure with
+    //   | Closure (f, x, fBody, fDeclEnv) ->
+    //     let xVal = Int(eval eArg env)
+    //     let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
+    //     eval fBody fBodyEnv
+    //   | _ -> failwith "eval Call: not a function"
+    | Call (Var f , eArgs) ->
       let fClosure = lookup env f
       match fClosure with
-      | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = Int(eval eArg env)
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
+      | Closure (f, xs, fBody, fDeclEnv) ->
+        let xVal = List.map (fun x -> Int (eval x env)) eArgs
+        let parameterEnv  = List.zip xs xVal
+        let fBodyEnv = parameterEnv @ ((f, fClosure) :: fDeclEnv)
         eval fBody fBodyEnv
-      | _ -> failwith "eval Call: not a function"
     | Call _ -> failwith "eval Call: not first-order function"
 
 (* Evaluate in empty environment: program must have no free variables: *)
@@ -71,46 +86,46 @@ let run e = eval e [];;
 
 (* Examples in abstract syntax *)
 
-let ex1 = Letfun("f1", "x", Prim("+", Var "x", CstI 1), 
-                 Call(Var "f1", CstI 12));;
+// let ex1 = Letfun("f1", "x", Prim("+", Var "x", CstI 1), 
+//                  Call(Var "f1", CstI 12));;
 
-(* Example: factorial *)
+// (* Example: factorial *)
 
-let ex2 = Letfun("fac", "x",
-                 If(Prim("=", Var "x", CstI 0),
-                    CstI 1,
-                    Prim("*", Var "x", 
-                              Call(Var "fac", 
-                                   Prim("-", Var "x", CstI 1)))),
-                 Call(Var "fac", Var "n"));;
+// let ex2 = Letfun("fac", "x",
+//                  If(Prim("=", Var "x", CstI 0),
+//                     CstI 1,
+//                     Prim("*", Var "x", 
+//                               Call(Var "fac", 
+//                                    Prim("-", Var "x", CstI 1)))),
+//                  Call(Var "fac", Var "n"));;
 
-(* let fac10 = eval ex2 [("n", Int 10)];; *)
+// (* let fac10 = eval ex2 [("n", Int 10)];; *)
 
-(* Example: deep recursion to check for constant-space tail recursion *)
+// (* Example: deep recursion to check for constant-space tail recursion *)
 
-let ex3 = Letfun("deep", "x", 
-                 If(Prim("=", Var "x", CstI 0),
-                    CstI 1,
-                    Call(Var "deep", Prim("-", Var "x", CstI 1))),
-                 Call(Var "deep", Var "count"));;
+// let ex3 = Letfun("deep", "x", 
+//                  If(Prim("=", Var "x", CstI 0),
+//                     CstI 1,
+//                     Call(Var "deep", Prim("-", Var "x", CstI 1))),
+//                  Call(Var "deep", Var "count"));;
     
-let rundeep n = eval ex3 [("count", Int n)];;
+// let rundeep n = eval ex3 [("count", Int n)];;
 
-(* Example: static scope (result 14) or dynamic scope (result 25) *)
+// (* Example: static scope (result 14) or dynamic scope (result 25) *)
 
-let ex4 =
-    Let("y", CstI 11,
-        Letfun("f", "x", Prim("+", Var "x", Var "y"),
-               Let("y", CstI 22, Call(Var "f", CstI 3))));;
+// let ex4 =
+//     Let("y", CstI 11,
+//         Letfun("f", "x", Prim("+", Var "x", Var "y"),
+//                Let("y", CstI 22, Call(Var "f", CstI 3))));;
 
-(* Example: two function definitions: a comparison and Fibonacci *)
+// (* Example: two function definitions: a comparison and Fibonacci *)
 
-let ex5 = 
-    Letfun("ge2", "x", Prim("<", CstI 1, Var "x"),
-           Letfun("fib", "n",
-                  If(Call(Var "ge2", Var "n"),
-                     Prim("+",
-                          Call(Var "fib", Prim("-", Var "n", CstI 1)),
-                          Call(Var "fib", Prim("-", Var "n", CstI 2))),
-                     CstI 1), Call(Var "fib", CstI 25)));;
+// let ex5 = 
+//     Letfun("ge2", "x", Prim("<", CstI 1, Var "x"),
+//            Letfun("fib", "n",
+//                   If(Call(Var "ge2", Var "n"),
+//                      Prim("+",
+//                           Call(Var "fib", Prim("-", Var "n", CstI 1)),
+//                           Call(Var "fib", Prim("-", Var "n", CstI 2))),
+//                      CstI 1), Call(Var "fib", CstI 25)));;
                      
